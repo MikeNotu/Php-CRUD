873bf9f9b81fbc2064769f5cebf7f8d09952663a
MikeNotu <mikebateman98@gmail.com>
2021-08-28 13:52:34 -0500

    Get, Post, Put, Delete

--- bd/BD.php
+++ bd/BD.php
@@ -19,4 +19,63 @@ function desconectar(){
     $GLOBALS['pdo']=null;
 }
 
+function metodoGet($query){
+    try{
+        conectar();
+        $sentencia=$GLOBALS['pdo']->prepare($query);
+        $sentencia=setFetchMode(PDO::FETCH_ASSOC);
+        $sentencia->execute();
+        desconectar();
+        return $sentencia;
+
+    }catch(Exception $e){
+        die("Error: ".$e);
+    }
+}
+
+function metodoPost($query, $queryAutoIncrement){
+    try{
+        conectar();
+        $sentencia=$GLOBALS['pdo']->prepare($query);
+        $sentencia->execute();
+        $idAutoIncrement=metodoGet($queryAutoIncrement)->fetch(PDO::FETCH_ASSOC);
+        $resultado=array_merge($idAutoIncrement, $_POST);
+        $sentenecia->closeCursor();
+        desconectar();
+        return $resultado;
+
+    }catch(Exception $e){
+        die("Error: ".$e);
+    }
+}
+
+function metodoPut($query){
+    try{
+        conectar();
+        $sentencia=$GLOBALS['pdo']->prepare($query);
+        $sentencia->execute();
+        $resultado=array_merge($_GET, $_POST);
+        $sentenecia->closeCursor();
+        desconectar();
+        return $resultado;
+
+    }catch(Exception $e){
+        die("Error: ".$e);
+    }
+}
+
+function metodoDelete($query){
+    try{
+        conectar();
+        $sentencia=$GLOBALS['pdo']->prepare($query);
+        $sentencia->execute();
+        $sentenecia->closeCursor();
+        desconectar();
+        return $_GET['id'];
+
+    }catch(Exception $e){
+        die("Error: ".$e);
+    }
+}
+
 ?>
